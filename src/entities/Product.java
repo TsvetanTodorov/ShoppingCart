a2fd494a10295c7f5da31680b95b7c189c1a4891
Tsvetan <ceckoyyyy@gmail.com>
2024-02-23 12:09:37 +0200

    Changes in the classes Product and ShopImpl

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,22 +2,103 @@ package entities;
 
 import exceptions.InvalidFieldException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Product {
 
     private String name;
     private double price;
     private int quantity;
 
-    public Product(String name, double price, int quantity) throws InvalidFieldException {
-        this.name = name;
+    public Product(String name, String price, String quantity) throws InvalidFieldException {
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+    public Product(String name, String quantity) throws InvalidFieldException {
+        setName(name);
+        setQuantity(quantity);
+    }
+
+
+    public void setName(String name) throws InvalidFieldException {
+        if (name == null || name.isBlank()) {
+            throw new InvalidFieldException("You cannot enter an empty name of the product!");
+        }
+
+        if (isValidName(name)) {
+            this.name = name;
+            return;
+        }
+
+        throw new InvalidFieldException("Invalid product name! Please enter a valid product name:");
+    }
+
+    public void setPrice(String price) throws InvalidFieldException {
+        if (price == null || price.isBlank()) {
+            throw new InvalidFieldException("You cannot enter an empty price for the product!");
+        }
+
+        if (isValidPrice(price)) {
+            this.price = Double.parseDouble(price);
+            return;
+        }
+
+        throw new InvalidFieldException("Invalid product price! Please enter a valid product price:");
+    }
+
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public void setQuantity(String quantity) throws InvalidFieldException {
+        if (quantity == null || quantity.isBlank()) {
+            throw new InvalidFieldException("You cannot enter an empty product quantity!");
+        }
+
+        if (isValidQuantity(quantity)) {
+            this.quantity = Integer.parseInt(quantity);
+        }
+
+        throw new InvalidFieldException("Invalid product quantity! Please enter a valid product quantity:");
+    }
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public Product(String name, int quantity) throws InvalidFieldException {
-        this.name = name;
-        this.quantity = quantity;
+    public String getName() {
+        return name;
     }
 
+    public double getPrice() {
+        return price;
+    }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    private boolean isValidName(String name) {
+        Pattern pattern = Pattern.compile("^(?:[a-zA-Z]+(?:-[a-zA-Z]+)?(?: [a-zA-Z]+)?)$");
+        Matcher matcher = pattern.matcher(name);
+
+        return matcher.matches();
+    }
+
+    private boolean isValidPrice(String price) {
+        Pattern pattern = Pattern.compile("^\\d+(?:\\.\\d{1,2})?$");
+        Matcher matcher = pattern.matcher(price);
+
+        return matcher.matches();
+    }
+
+    private boolean isValidQuantity(String quantity) {
+        Pattern pattern = Pattern.compile("^(?![0]$\\d{1,4}$)");
+        Matcher matcher = pattern.matcher(quantity);
+
+        return matcher.matches();
+    }
 }
--- src/entities/ShopImpl.java
+++ src/entities/ShopImpl.java
@@ -1,4 +1,104 @@
 package entities;
 
-public class ShopImpl {
+import exceptions.InvalidFieldException;
+import interfaces.Shop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShopImpl implements Shop {
+
+    private Map<String, Product> products = new HashMap<>();
+
+    public ShopImpl() throws InvalidFieldException {
+        products.put("milk", new Product("Milk", "2.50", "10"));
+        products.put("bread", new Product("Bread", "1.50", "15"));
+        products.put("egg", new Product("Egg", "0.50", "100"));
+    }
+
+    @Override
+    public void addProduct(Product newProduct) throws InvalidFieldException {
+        String newProductName = newProduct.getName();
+
+        if (products.containsKey(newProductName)) {
+            setNewProductInfo(newProduct);
+        } else {
+            products.put(newProductName, newProduct);
+        }
+
+        System.out.printf("Successfully added the product: %s, with quantity: %d and price: %.2f to the shop!\n",
+                newProductName, newProduct.getQuantity(), newProduct.getPrice());
+    }
+
+    @Override
+    public void buyProduct(Product productToBuy) throws InvalidFieldException {
+        if (isExistingProduct(productToBuy)) {
+            throw new InvalidFieldException("Sorry we don't have this product in the shop!");
+        }
+
+        String productToBuyName = productToBuy.getName();
+        Product existingProduct = products.get(productToBuyName.toLowerCase());
+
+        if (hasEnoughQuantity(existingProduct, productToBuy)) {
+            throw new InvalidFieldException("Sorry we don't have that many " + productToBuyName);
+        }
+
+        existingProduct.setQuantity(existingProduct.getQuantity() - productToBuy.getQuantity());
+
+        System.out.printf("You successfully added %s to your shopping cart!\n", productToBuyName);
+        System.out.println("Would you like to buy anything else ?");
+    }
+
+    @Override
+    public void returnProduct(Product product) {
+        if (isExistingProduct(product)) {
+            products.put(product.getName(), product);
+        } else {
+            String productToAdd = product.getName();
+            int quantityToAdd = product.getQuantity();
+            Product currentProduct = products.get(productToAdd.toLowerCase());
+
+            int totalQuantity = quantityToAdd + currentProduct.getQuantity();
+
+            currentProduct.setQuantity(totalQuantity);
+        }
+    }
+
+    @Override
+    public void checkProductAvailability(String name) {
+        if (products.containsKey(name)) {
+            Product currentProduct = products.get(name);
+            System.out.printf("Yes! We have the product: %s, with %d available quantity!\n",
+                    name, currentProduct.getQuantity());
+        } else {
+            System.out.printf("Sorry! We don't have the product: %s\n", name);
+        }
+    }
+
+    @Override
+    public void checkAllProductsAvailability() {
+
+    }
+
+    private void setNewProductInfo(Product newProduct) {
+        Product currentProduct = products.get(newProduct.getName().toLowerCase());
+
+        int totalQuantity = newProduct.getQuantity() + currentProduct.getQuantity();
+        double totalCost = (newProduct.getPrice() * newProduct.getQuantity())
+                + (currentProduct.getPrice() * currentProduct.getQuantity());
+        double averagePrice = totalCost / totalQuantity;
+
+        currentProduct.setQuantity(totalQuantity);
+        currentProduct.setPrice(averagePrice);
+    }
+
+    private boolean isExistingProduct(Product product) {
+        return !products.containsKey(product.getName());
+    }
+
+    private boolean hasEnoughQuantity(Product existingProduct, Product productToBuy) {
+        int quantityToBuy = productToBuy.getQuantity();
+
+        return existingProduct.getQuantity() < quantityToBuy;
+    }
 }
