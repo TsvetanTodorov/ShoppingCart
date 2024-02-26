2d4a019f2bc173f3561a239fde33c4b92283a420
Tsvetan <tstodorofff@gmail.com>
2024-02-26 11:57:15 +0200

    Adding CommandService class

---
+++ src/services/CommandService.java
@@ -0,0 +1,81 @@
+package services;
+
+import entities.CustomerImpl;
+import entities.Product;
+import entities.ShopImpl;
+import exceptions.InvalidFieldException;
+
+import java.util.Scanner;
+
+public class CommandService {
+    private ShopImpl shop;
+    private CustomerImpl customer;
+
+    public CommandService(ShopImpl shop, CustomerImpl customer) {
+        this.shop = shop;
+        this.customer = customer;
+    }
+
+    public void runCommands(String command, Scanner scanner) throws InvalidFieldException {
+        while (!command.equals("Cash Out")) {
+            Product product;
+
+            String[] commandArr = command.split("-");
+            String commandType = commandArr[0];
+            switch (commandType) {
+                case "add":
+                    String productName = commandArr[1];
+                    String quantity = commandArr[2];
+                    String price = commandArr[3];
+                    try {
+                        product = new Product(productName, price, quantity);
+                        shop.addProduct(product);
+                    } catch (InvalidFieldException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "check":
+                    customer.checkShoppingCart();
+                    break;
+                case "check all":
+                    shop.checkAllProductsAvailability();
+                    break;
+                case "return":
+                    productName = commandArr[1];
+                    quantity = commandArr[2];
+                    try {
+                        customer.returnProduct(new Product(productName, quantity));
+                    } catch (InvalidFieldException e) {
+                        System.out.println(e.getMessage());
+                        break;
+                    }
+                    shop.returnProduct(new Product(productName, quantity));
+                    break;
+                case "buy":
+                    try {
+                        productName = commandArr[1];
+                        quantity = commandArr[2];
+                        Product currentProduct = shop.getProduct(productName);
+                        if (currentProduct == null) {
+                            break;
+                        }
+                        shop.buyProduct(new Product(productName, quantity));
+                        Product productToBuy = new Product(productName, String.valueOf(currentProduct.getPrice()), quantity);
+                        customer.buyProduct(productToBuy);
+                    } catch (InvalidFieldException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "help":
+                    customer.help();
+                    break;
+                case "cash out":
+                    customer.cashOut();
+                    return;
+            }
+
+            command = scanner.nextLine();
+        }
+    }
+
+}
