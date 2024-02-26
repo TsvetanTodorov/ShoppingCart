5314e0c63fdcd2769d2770fc04b316020f1bec00
Tsvetan <tstodorofff@gmail.com>
2024-02-26 11:56:58 +0200

    Adding ShoppingConstants interface

---
+++ src/Constants/ShoppingConstants.java
@@ -0,0 +1,26 @@
+package Constants;
+
+public interface ShoppingConstants {
+
+    String[] commandsArr = {
+            "add",
+            "buy",
+            "return",
+            "check",
+            "check all",
+            "help",
+            "cash out"
+    };
+
+    String[] commandsDescription = {
+            "Add some products to the shop! Example add-juice-10-2.50",
+            "Buy Product -> Example: buy-milk-2",
+            "Return Product -> Example: return-milk-1",
+            "Check what's in your shopping cart: Example: check",
+            "Check what's available in the shop: Example: check all",
+            "If you want to check all these commands again just type help",
+            "If you're done with your shopping type cash out"
+    };
+
+
+}
