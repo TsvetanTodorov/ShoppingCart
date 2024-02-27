67c0db8c1c2b39cfab6023e7c9d38fa8a467837f
Tsvetan <tstodorofff@gmail.com>
2024-02-27 15:36:48 +0200

    Refactoring code in the String[] commandsDescription in thе interface ShoppingConstants

--- src/constants/ShoppingConstants.java
+++ src/constants/ShoppingConstants.java
@@ -13,11 +13,11 @@ public interface ShoppingConstants {
     };
 
     String[] commandsDescription = {
-            "Add some products to the shop! Example add-juice-10-2.50",
+            "Add some products to the shop -> Example add-juice-10-2.50",
             "Buy Product -> Example: buy-milk-2",
             "Return Product -> Example: return-milk-1",
-            "Check what's in your shopping cart: Example: check",
-            "Check what's available in the shop: Example: check all",
+            "Check what's in your shopping cart -> Example: check",
+            "Check what's available in the shop -> Example: check all",
             "If you want to check all these commands again just type help",
             "If you're done with your shopping type cash out"
     };
